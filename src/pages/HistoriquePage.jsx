e61d3c6015440fb0985adfe130a78215f521dca4
LOUIS Pieddr-Andre <webdeveloper3030@gmail.com>
2025-02-11 11:23:35 -0500

    geofence add 3333

--- src/pages/HistoriquePage.jsx
+++ src/pages/HistoriquePage.jsx
@@ -150,7 +150,7 @@ function HistoriquePage() {
   useEffect(() => {
     // setvehicles(véhiculeData);
     if (centrerAutoMapTrajet) {
-      centerOnFirstMarker();
+      // centerOnFirstMarker();
     }
   }, [véhiculeHistoriqueDetails, vehicles, currentVéhicule]);
 
