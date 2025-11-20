c53da3f7471ec72b85bfca1a5a9439cc9cf968b9
LOUIS Pieddr-Andre <webdeveloper3030@gmail.com>
2025-11-20 13:05:30 -0500

    correction transfert appareil22

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -18,7 +18,7 @@ import { debounce } from "lodash";
 export const DataContext = createContext();
 
 const DataContextProvider = ({ children }) => {
-  let versionApplication = "1.1.8";
+  let versionApplication = "1.1.9";
   let x;
   const navigate = useNavigate();
   const [t, i18n] = useTranslation();
@@ -5848,7 +5848,7 @@ const DataContextProvider = ({ children }) => {
     // const url = `https://www.octagonoplus.com/changer-appareil-compte-api/services/changeAccount.php?imei=${imei}&compte=${newCompte}`;
 
     // const url = `/changer-appareil-compte-api/services/changeAccount.php?imei=${imei}&compte=${newCompte}`;
-    const url = `https://octagonoplus.com/changer-appareil-compte-api/services/changeAccount.php?imei=${imei}&compte=${newCompte}`;
+    const url = `https://octagonoplus.com/changer-appareil-compte-api/services/changeAccountAPP.php?imei=${imei}&compte=${newCompte}`;
 
     try {
       const res = await fetch(url, {
